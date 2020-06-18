b032e638f2514e7545a1ba3adc5e7a1e561e6ffa
Nguyen Chinh Huu <huupoke12@gmail.com>
2020-06-18 23:01:01 +0700

    Polish and rename setup page, add newlines

--- public/include/header.php
+++ public/include/header.php
@@ -1,5 +1,5 @@
 <?php
-$primaryColor = 'teal';
+define('PRIMARY_COLOR', 'teal');
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +10,6 @@ $primaryColor = 'teal';
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" >
-    <!-- Additional tags here -->
     <?php
     if (function_exists('customPageHeader')){
       customPageHeader();
@@ -20,9 +19,9 @@ $primaryColor = 'teal';
   </head>
 
   <body>
-    <nav class="w3-bar w3-<?php echo $primaryColor; ?>">
+    <nav class="w3-bar w3-<?php echo PRIMARY_COLOR; ?>">
       <a href="/" class="w3-bar-item w3-button" title="Trang chủ"><i class="material-icons">home</i> Trang chủ</a>
-      <a href="/admin/setup-database.php" class="w3-bar-item w3-button" title="Cài đặt cơ sở dữ liệu"><i class="material-icons">settings</i> Cài đặt cơ sở dữ liệu</a>
+      <a href="/admin/setup.php" class="w3-bar-item w3-button" title="Cài đặt"><i class="material-icons">settings</i> Cài đặt</a>
     </nav>
 
     <div class="w3-container">
