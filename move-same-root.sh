f1cddb4d96dcbb1829b4df7f11e4aad2e9cab39d
Nguyen Chinh Huu <huupoke12@gmail.com>
2020-07-08 16:16:24 +0700

    Remove unneccesary files

--- move-same-root.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Run this script from the project root
-
-find 'public' -maxdepth 1 -exec mv "{}" . \;
-rmdir 'public'
-
-find . -type f -name '*.php' -exec sed -i 's/dirname($_SERVER\['\''DOCUMENT_ROOT'\''\])/$_SERVER\['\''DOCUMENT_ROOT'\''\]/g' "{}" \;
-
