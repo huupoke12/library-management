fa920cf5c435a8c689def33a285f8b845a210133
Nguyễn Chính Hữu <huupoke12@gmail.com>
2020-07-10 01:43:46 +0700

    Use custom stylesheet

--- include/footer.php
+++ include/footer.php
@@ -2,13 +2,13 @@
 
     <footer class="w3-panel w3-<?php echo PRIMARY_COLOR; ?>">
       <p class="w3-large w3-center">
-        <b><time datetime="<?php echo date('Y-m-d') . 'T' . date('H:iP'); ?>">
+        <time datetime="<?php echo date('Y-m-d') . 'T' . date('H:iP'); ?>">
           <i class="material-icons" title="Giờ">schedule</i>
           <?php echo date('H:i') . ' (' . date('P') . ')'; ?>
           <br>
           <i class="material-icons" title="Ngày">today</i>
           <?php echo date('d/m/Y');?>
-          </time></b>
+          </time>
       </p>
     </footer>
   </body>
--- include/header.php
+++ include/header.php
@@ -10,7 +10,8 @@ require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/include/environment-config.p
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" >
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="/style/custom.css">
     <?php
     if (function_exists('customPageHeader')){
       customPageHeader();
