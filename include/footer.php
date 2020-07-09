4ceccc8d18277a702665da6b6b498a5c58dabcb5
Nguyen Chinh Huu <huupoke12@gmail.com>
2020-07-09 18:15:36 +0700

    Adjust footer

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,9 @@
     </main>
 
-    <footer class="w3-panel w3-large w3-center w3-<?php echo PRIMARY_COLOR; ?>">
-    Bây giờ là <b><?php echo date('H:i'); ?></b> ngày <b><?php echo date('d/m/Y') . ' (' . date('P') . ')'; ?></b>
+    <footer class="w3-panel w3-<?php echo PRIMARY_COLOR; ?>">
+      <p class="w3-large w3-center">
+        Bây giờ là <b><?php echo date('H:i'); ?></b> ngày <b><?php echo date('d/m/Y') . ' (' . date('P') . ')'; ?></b>
+      </p>
     </footer>
   </body>
 </html>
